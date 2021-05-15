f3b4c9e24082eb1837e44fdc89eb8e0432d5c7b1
Yehia Khalil <yehiakhalilykk@gamil.com>
2021-05-16 00:19:55 +0200

    added some validations and created signup form

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,47 +1,73 @@
 require 'bcrypt'
 class Api::V1::UsersController < ApplicationController
     include BCrypt
-    before_action :authorized, only: [:auto_login]
+    # before_action :authorized, only: [:auto_login]
 
   # REGISTER
   def create
-    @pass = BCrypt::Password.create(user_params["password_digest"])
-    @user = User.new(username: user_params["username"], password_digest: @pass, email:user_params["email"] )
-    if @user.valid?
-      @user.save
-      token = encode_token({user_id: @user.id})
-      render json: {
-        success:true, 
-        data: UserSerializer.new(@user), 
-        token: token}, 
-        status:201
+    @test1 = User.find_by(username: params[:username])
+    @test2 = User.find_by(email: params[:email])
+    
+    if !@test1 && !@test2 
+      @test3 = params[:password] ? params[:password].length>6 : ""
+
+      # User passes validations
+      @user = User.new(user_params)
+      if @user.valid? && user_params["password"] == user_params["password_confirmation"] && @test3
+        @user.save
+        token = encode_token({user_id: @user.id})
+        render json: {
+          success:true, 
+          data: UserSerializer.new(@user), 
+          token: token}, 
+          status:201
+      else
+        !@test3 ? @user.errors.messages["Password length"] = "Password must be greater than 6 characters": nil
+        render json: {
+          success: false, 
+          error: @user.errors,
+          status:"400"
+        }, status: 400
+      end
     else
+      # Database errors occur
+      @msg= {}
+      if @test1
+        @msg["username"] = "Username already exists"
+      end
+      if @test2
+        @msg["email"] = "Email already exists"
+      end
       render json: {
         success: false, 
-        error: "Invalid email or password"},
-        status: 400
+        error: @msg,
+        status:"400"
+      }, status: 400
     end
-    rescue Exception => e
-      render json: {
-        success: false,
-        errors: e.message
-      }, status: 500
+      
+    # rescue Exception => e
+    #   render json: {
+    #     success: false,
+    #     error: e.message,
+    #     status:"500"
+    #     }, status: 500
+    # render json: {params: params}
   end
 
   # LOGGING IN
   def login
     @user = User.find_by(email: params[:email])
-    if @user && BCrypt::Password.new(@user.password_digest) == params[:password_digest]
+    if @user && BCrypt::Password.new(@user.password_digest) == params[:password]
       token = encode_token({user_id: @user.id})
       render json: {
         success: true, 
         data: UserSerializer.new(@user), 
         token: token}, 
-        status: 201
+        status: 200
     else
       render json: {
         success: false,
-        error: "Invalid email or password"},
+        error: "Invalid Username or Password"},
         status: 400
     end
   end
@@ -54,6 +80,6 @@ class Api::V1::UsersController < ApplicationController
   private
 
   def user_params
-    params.permit(:username, :password_digest, :email)
+    params.permit(:username, :password, :password_confirmation, :email)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
+    has_secure_password
     validates :username, presence: true 
     validates :email, presence: true
-    validates :password_digest, presence: true, confirmation: true
+    validates :password_confirmation, presence: true
 end
