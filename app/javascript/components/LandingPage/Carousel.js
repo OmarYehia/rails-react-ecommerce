f6e74912c9aa8440458bcb7d22b9fc0546efdeca
Omar Yehia <oyehia94@gmail.com>
2021-05-17 21:12:56 +0200

    Landing page carousel completed

---
+++ app/javascript/components/LandingPage/Carousel.js
@@ -0,0 +1,53 @@
+import React from "react";
+import Slider from "infinite-react-carousel";
+import { Link } from "react-router-dom";
+
+const Carousel = () => {
+  const settings = {
+    arrowsBlock: true,
+    autoplay: true,
+    autoplaySpeed: 6000,
+    duration: 100,
+  };
+
+  return (
+    <div className="carousel-container">
+      <Slider {...settings} className="carousel">
+        <div className="row m-0">
+          <div className="col-7 m-0 d-flex flex-column align-items-center justify-content-center">
+            <h2>Browse Categories</h2>
+            <p>Get your best deal today ... </p>
+            <Link to="/categories" className="btn btn-primary btn-large">
+              {" "}
+              Shop Now
+            </Link>
+          </div>
+          <div className="col-5 m-0 d-flex flex-column align-items-center justify-content-center">
+            <img
+              src="https://90shopping.co/wp-content/uploads/2021/02/girl-shopping-png-3.png"
+              className="carousel-img"
+            />
+          </div>
+        </div>
+        <div className="row m-0">
+          <div className="col-7 d-flex flex-column align-items-center justify-content-center">
+            <h2>Browse Categories</h2>
+            <p>Get your best deal today ... </p>
+            <Link to="/categories" className="btn btn-primary btn-large">
+              {" "}
+              Shop Now
+            </Link>
+          </div>
+          <div className="col-5 d-flex flex-column align-items-center justify-content-center">
+            <img
+              src="https://img2.pngio.com/working-man-png-2-png-image-working-man-png-697_423.png"
+              className="carousel-img"
+            />
+          </div>
+        </div>
+      </Slider>
+    </div>
+  );
+};
+
+export default Carousel;
--- app/javascript/components/LandingPage/LandingPage.js
+++ app/javascript/components/LandingPage/LandingPage.js
@@ -1,10 +1,12 @@
 import React from "react";
 import CategoryList from "../Category/CategoryList/CategoryList";
+import Carousel from "./Carousel";
 import "./LandingPage.css";
 
 const LandingPage = () => {
   return (
     <div>
+      <Carousel />
       <div className="featured-categories">
         <CategoryList title="Featured Categories" featured="3" />
       </div>
