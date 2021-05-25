5887cc717c508759dd65936613ee3cd4536e60c1
AmiraSakr-Intake41 <amira.21sakr@gmail.com>
2021-05-25 21:36:44 +0200

    after making migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2021_05_25_172020) do
 
   # These are extensions that must be enabled in order to support this database
