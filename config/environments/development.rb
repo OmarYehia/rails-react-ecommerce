16c126640a8b12fb7dd6c268b7ef18738de166f4
Yehia Khalil <yehiakhalilykk@gamil.com>
2021-05-26 02:11:11 +0200

    yarab

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,6 +9,7 @@ Rails.application.configure do
 
   # Do not eager load code on boot.
   config.eager_load = false
+  config.action_mailer.default_url_options = { :host => "https://rails-react-ecommerce.herokuapp.com" }
 
   # Show full error reports.
   config.consider_all_requests_local = true
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,6 +9,7 @@ Rails.application.configure do
   # and those relying on copy on write to perform better.
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
+  config.action_mailer.default_url_options = { :host => "https://rails-react-ecommerce.herokuapp.com" }
 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
@@ -27,7 +28,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
